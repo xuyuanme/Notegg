a15df748cb0b43a1bf4874446c9edc57e84989b3
xuyuanme <xuyuanme@gmail.com>
2014-01-12 10:30:59 +0800

    integrate angularjs and dropbox.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,10 +3,33 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('MyCtrl', ['$scope', '$location', function ($scope, $location) {
+    .controller('MyCtrl', ['$scope', '$location', 'DropBoxService', function ($scope, $location, DropBoxService) {
         $scope.goto = function (value) {
             $location.path(value);
-//            window.alert($scope.actualWidth + ',' + $scope.actualHeight + ',' + $scope.screenWidth + ',' + $scope.screenHeight);
+            // window.alert($scope.actualWidth + ',' + $scope.actualHeight + ',' + $scope.screenWidth + ',' + $scope.screenHeight);
+        };
+
+        $scope.authDropbox = function () {
+            DropBoxService.authenticate(function (err, client) {
+                if (err) {
+                    console.log('err: ' + err);
+                    DropBoxService.reset();
+                } else {
+                    console.log('auth ok');
+                    $scope.readNotes();
+                    console.log('requested read notes');
+                }
+            });
+        };
+
+        $scope.readNotes = function () {
+            // $scope.notes = DropBoxService.readNotes();
+            DropBoxService.readNotes().then(function (notes) {
+                $scope.notes = notes;
+                console.log('read notes done');
+            }, function (error) {
+                console.log('read notes promise get error: ' + error);
+            });
         }
     }])
     .controller('MyCtrl1', [function () {
--- www/js/services.js
+++ www/js/services.js
@@ -6,4 +6,59 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
-    .value('version', '0.1');
+    .value('version', '0.1')
+    .factory('DropBoxService', function ($q) {
+        console.log('init dropbox client');
+        var client = new Dropbox.Client({ key: 'w7hk0g1c2pnqs8g' });
+        if (myApp.isPhone) {
+            client.authDriver(new Dropbox.AuthDriver.Cordova({rememberUser: true}));
+        } else {
+            client.authDriver(new Dropbox.AuthDriver.Popup({ rememberUser: true, receiverUrl: document.location + 'auth_receiver.html' }));
+        }
+
+        var service = {
+            authenticate: function (fn) {
+                console.log('start auth');
+                console.log(client._oauth._token);
+                if (client._oauth._token === null) {
+                    console.log('auth token is null, client reset');
+                    this.reset();
+                }
+                try {
+                    client.authenticate(fn);
+                } catch (err) {
+                    console.log('catch err: ' + err);
+                    this.reset();
+                }
+            },
+            reset: function () {
+                console.log('client reset');
+                client.reset();
+            },
+            readNotes: function () {
+                console.log('start read notes');
+                var defered = $q.defer();
+                client.readFile("notes.txt", function (err, data) {
+                    if (err) {
+                        console.log('read notes err: ' + err);
+                        defered.reject(err);
+                    } else {
+                        console.log('read notes resolved');
+                        defered.resolve(data);
+                    }
+                });
+                return defered.promise;
+            }
+//            writeNotes: function () {
+//                client.writeFile('notes.txt', 'Hello, World!a', function (error) {
+//                    if (error) {
+//                        window.alert('Error: ' + error);
+//                    } else {
+//                        window.alert('File written successfully!');
+//                    }
+//                });
+//            }
+        };
+
+        return service;
+    });
