3c18e956a19daef8c3f0b1ad5ce75ea9146c2a13
xuyuanme <xuyuanme@gmail.com>
2014-01-12 14:57:46 +0800

    disable html5mode for phonegap

--- www/js/app.js
+++ www/js/app.js
@@ -11,7 +11,10 @@ var myApp = angular.module('myApp', [
         'myApp.controllers'
     ]).
     config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true).hashPrefix('!');
+        if (document.URL.indexOf('https://') !== -1 || document.URL.indexOf('http://') !== -1) {
+            // if not phonegap, use html5mode
+            $locationProvider.html5Mode(true).hashPrefix('!');
+        }
         $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
         $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl1'});
         $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MyCtrl'});
