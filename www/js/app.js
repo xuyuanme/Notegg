c1860aef861add9ed8cb435eac585a24f916b16c
xuyuanme <xuyuanme@gmail.com>
2014-01-14 11:23:23 +0800

    add ionic framework

--- www/js/app.js
+++ www/js/app.js
@@ -17,6 +17,7 @@ var myApp = angular.module('myApp', [
         }
         $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
         $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/uitest', {templateUrl: 'partials/uitest.html', controller: 'MyCtrl1'});
         $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MyCtrl'});
 
         $routeProvider.otherwise({redirectTo: '/'});
