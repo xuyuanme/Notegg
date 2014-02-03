94d1d1e704de7bf1580a3b3a4809fef35ee19987
xuyuanme <xuyuanme@gmail.com>
2014-02-03 15:08:35 +0800

    using https://github.com/akatov/angular-contenteditable

--- www/js/directives.js
+++ www/js/directives.js
@@ -9,35 +9,96 @@ angular.module('myApp.directives', [])
             elm.text(version);
         };
     }])
-    .directive('contenteditable', function () {
+    .directive('contenteditable', ['$timeout', function ($timeout) {
         return {
-            restrict: 'A', // only activate on element attribute
-            require: '?ngModel', // get a hold of NgModelController
+            restrict: 'A',
+            require: '?ngModel',
             link: function (scope, element, attrs, ngModel) {
-                if (!ngModel) return; // do nothing if no ng-model
+                // don't do anything unless this is actually bound to a model
+                if (!ngModel) {
+                    return
+                }
 
-                // Specify how UI should be updated
+                // options
+                var opts = {}
+                angular.forEach([
+                    'stripBr',
+                    'noLineBreaks',
+                    'selectNonEditable',
+                    'moveCaretToEndOnChange',
+                ], function (opt) {
+                    var o = attrs[opt]
+                    opts[opt] = o && o !== 'false'
+                })
+
+                // view -> model
+                element.bind('input', function (e) {
+                    scope.$apply(function () {
+                        var html, html2, rerender
+                        html = element.html()
+                        rerender = false
+                        if (opts.stripBr) {
+                            html = html.replace(/<br>$/, '')
+                        }
+                        if (opts.noLineBreaks) {
+                            html2 = html.replace(/<div>/g, '').replace(/<br>/g, '').replace(/<\/div>/g, '')
+                            if (html2 !== html) {
+                                rerender = true
+                                html = html2
+                            }
+                        }
+                        ngModel.$setViewValue(html)
+                        if (rerender) {
+                            ngModel.$render()
+                        }
+                        if (html === '') {
+                            // the cursor disappears if the contents is empty
+                            // so we need to refocus
+                            $timeout(function () {
+                                element[0].blur()
+                                element[0].focus()
+                            })
+                        }
+                    })
+                })
+
+                // model -> view
+                var oldRender = ngModel.$render
                 ngModel.$render = function () {
-                    element.html(ngModel.$viewValue || '');
-                };
-
-                // Listen for change events to enable binding
-                element.on('blur keyup change', function () {
-                    scope.$apply(read);
-                });
-                read(); // initialize
-
-                // Write data to the model
-                function read() {
-                    var html = element[0].innerText;
-                    // When we clear the content editable the browser leaves a <br> behind
-                    // If strip-br attribute is provided then we strip this out
-//                    if (attrs.stripBr && html == '<br>') {
-//                        html = '';
-//                    }
-                    ngModel.$setViewValue(html);
+                    var el, el2, range, sel
+                    if (!!oldRender) {
+                        oldRender()
+                    }
+                    element.html(ngModel.$viewValue || '')
+                    if (opts.moveCaretToEndOnChange) {
+                        el = element[0]
+                        range = document.createRange()
+                        sel = window.getSelection()
+                        if (el.childNodes.length > 0) {
+                            el2 = el.childNodes[el.childNodes.length - 1]
+                            range.setStartAfter(el2)
+                        } else {
+                            range.setStartAfter(el)
+                        }
+                        range.collapse(true)
+                        sel.removeAllRanges()
+                        sel.addRange(range)
+                    }
+                }
+                if (opts.selectNonEditable) {
+                    element.bind('click', function (e) {
+                        var range, sel, target
+                        target = e.toElement
+                        if (target !== this && angular.element(target).attr('contenteditable') === 'false') {
+                            range = document.createRange()
+                            sel = window.getSelection()
+                            range.setStartBefore(target)
+                            range.setEndAfter(target)
+                            sel.removeAllRanges()
+                            sel.addRange(range)
+                        }
+                    })
                 }
             }
-        };
-    });
-;
+        }
+    }]);
